dcb5d9a8b7b752854d92f3faedc96a558b9c93bb
Jeffy Arias <jeffreyarias21@gmail.com>
2020-01-04 19:11:45 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,26 @@ import Navar from './components/navbar';
 import Footer from './components/footer';
 import Booking from './components/Booking';
 import Payments from './components/Payments';
-
-
-
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-
+//import BookingModal2 from './components/BookingModal2';
+//import Modal from './components/modal';
 function App() {
   return (
+    <BrowserRouter>
     <div>
      <Navar />
      <Slider />
      <Booking />
-     <Payments />
      <Footer />
+     
       
 
 
 
 
     </div>
+   </BrowserRouter>
   );
 }
 
